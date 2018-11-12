2c232a11a2659d0c65932e9768def0801a30a32f
v.nesterenko <vith77@gmail.com>
2018-11-12 07:01:37 +0300

    Правки

--- utils/date.js
+++ utils/date.js
@@ -48,5 +48,32 @@ export default {
         const month = date.getMonth();
         const year = date.getFullYear();
         return `${months[month]} ${year}`;
+    },
+    compareDatesWithoutTime: function (d1, d2) {
+        let date1;
+        let date2;
+
+        if (typeof d1 === "string") {
+            date1 = new Date(d1);
+        } else {
+            date1 = d1;
+        }
+        
+        if (typeof d2 === "string") {
+            date2 = new Date(d2);
+        } else {
+            date2 = d2;
+        }
+
+        const day1 = date1.getDate();
+        const month1 = date1.getMonth();
+        const year1 = date1.getFullYear();
+
+        const day2 = date2.getDate();
+        const month2 = date2.getMonth();
+        const year2 = date2.getFullYear();
+
+        return (day1 === day2) && (month1 === month2) && (year1 === year2);
+
     }
 };
